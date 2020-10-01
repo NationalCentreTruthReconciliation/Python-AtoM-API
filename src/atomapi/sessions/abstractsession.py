033c74ce44462656049f32986354ac28ac888987
Daniel Lovegrove <Daniel.Lovegrove@umanitoba.ca>
2020-09-30 20:20:03 -0500

    Add more properties to AbstractSession

--- src/atomapi/sessions/abstractsession.py
+++ src/atomapi/sessions/abstractsession.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
+from atomapi.utils import parse_url_from_string
 
 class AbstractSession(ABC):
     def __init__(self, url: str, cache_credentials: bool = False):
-        self.url = url.rstrip("/")
+        parsed_url = parse_url_from_string(url.rstrip("/"))
+        self.host = parsed_url.host
+        self.url = str(parsed_url)
         self.cache_credentials = cache_credentials
+        self.session_is_authorized = False
+        self.__auth_session = None
 
     @abstractmethod
     def get_authorized_session(self):
