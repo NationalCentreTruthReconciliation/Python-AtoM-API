1d7b38c11f54620d3c9582807be282d5623156e1
Daniel Lovegrove <Daniel.Lovegrove@umanitoba.ca>
2020-10-02 12:41:55 -0500

    Separate virtual endpoints to a new file

--- src/atomapi/taxonomies.py
+++ src/atomapi/taxonomies.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-
 class DefaultTaxonomyIds(Enum):
     ''' It is possible to change what IDs AtoM uses for these taxonomies, so use these only if
     you're sure the IDs have not been changed.
@@ -19,62 +18,3 @@ class DefaultTaxonomyIds(Enum):
     DACS_NOTE_TYPE = 74
     RIGHTS_ACT = 67
     RIGHTS_BASIS = 68
-
-
-def _get_generic_taxonomies_from_soup(soup):
-    for element in soup.find_all('td'):
-        anchor_tag = element.find('a')
-        if anchor_tag:
-            yield {'name': anchor_tag.string}
-
-def _get_authorities_from_soup(soup):
-    for element in soup.find_all('p', class_='title'):
-        anchor_tag = element.find('a')
-        if anchor_tag:
-            yield {'name': anchor_tag.string}
-
-def _get_authorities_from_soup_with_ref_codes(soup):
-    for element in soup.find_all('article', class_='search-result'):
-        authority_name = ''
-        reference_code = ''
-
-        title_anchor_tag = element.find('p', class_='title').find('a')
-        if not title_anchor_tag:
-            continue
-        else:
-            authority_name = title_anchor_tag.string
-
-        ref_code_li_tag = element.find('li', class_='reference-code')
-        if ref_code_li_tag:
-            reference_code = ref_code_li_tag.string
-
-        yield {'name': authority_name, 'code': reference_code}
-
-# Add new virtual taxonomies here
-virtual_taxonomies = {
-    42: {
-        'description': 'Gets all place taxonomies',
-        'scrape_url': '{atom_url}/taxonomy/index/id/{id}?page={page}&limit={limit}',
-        'parser': _get_generic_taxonomies_from_soup,
-    },
-    35: {
-        'description': 'Gets all subject taxonomies',
-        'scrape_url': '{atom_url}/taxonomy/index/id/{id}?page={page}&limit={limit}',
-        'parser': _get_generic_taxonomies_from_soup,
-    },
-    78: {
-        'description': 'Gets all genre taxonomies',
-        'scrape_url': '{atom_url}/taxonomy/index/id/{id}?page={page}&limit={limit}',
-        'parser': _get_generic_taxonomies_from_soup,
-    },
-    101: {
-        'description': 'Gets all authority names',
-        'scrape_url': '{atom_url}/actor/browse?page={page}&limit={limit}',
-        'parser': _get_authorities_from_soup,
-    },
-    102: {
-        'description': 'Gets all authority names and their reference codes (if they exist)',
-        'scrape_url': '{atom_url}/actor/browse?page={page}&limit={limit}',
-        'parser': _get_authorities_from_soup_with_ref_codes
-    },
-}
