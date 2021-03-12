5284c137948eb6b2ff8632946b5c5f7c618b3004
Daniel Lovegrove <Daniel.Lovegrove@umanitoba.ca>
2021-03-12 09:22:19 -0600

    Be lazier - load Models and Authorizer lazily

--- src/atomapi/atom.py
+++ src/atomapi/atom.py
@@ -6,29 +6,49 @@ from atomapi.authorizer import Authorizer, BasicAuthorizer
 
 
 class Atom:
-    def __init__(self, url: str, api_key: str, **kwargs):
+    def __init__(self, url: str, api_key: str = None, authorizer=None, **kwargs):
         parsed_url = parse_url_from_string(url.rstrip("/"))
         self.host = parsed_url.host
         self.url = str(parsed_url)
-        self.api_key = api_key
+        self.api_key = api_key or ''
 
         self._lazy_session = None
-        self._authorizer = BasicAuthorizer(self.url)
-
-        self.taxonomies = Taxonomy(self)
-        self.informationobjects = InformationObject(self)
+        self._authorizer = authorizer
+        self._taxonomies = None
+        self._informationobjects = None
 
     def set_authorizer(self, authorizer: Authorizer):
         self._authorizer = authorizer
 
+    def set_api_key(self, key: str):
+        self.api_key = key
+
+    @property
+    def taxonomies(self) -> Taxonomy:
+        ''' Access taxonomies with the browse() method. '''
+        if self._taxonomies is None:
+            self._taxonomies = Taxonomy(self)
+        return self._taxonomies
+
+    @property
+    def informationobjects(self) -> InformationObject:
+        ''' Read information objects with the read() method, or search for objects with the
+        browse() method.
+        '''
+        if self._informationobjects is None:
+            self._informationobjects = InformationObject(self)
+        return self._informationobjects
+
     @property
     def _session(self):
         if self._lazy_session is None:
+            if self._authorizer is None:
+                self._authorizer = BasicAuthorizer(self.url)
             self._lazy_session = self._authorizer.authorize()
         return self._lazy_session
 
     def get(self, path: str, params: dict = None, sf_culture: str = 'en') -> tuple:
-        ''' Make a request to the AtoM API.
+        ''' Make a request to the AtoM site.
 
         Args:
             path (str): The URL path to access. The path does not include the base URL
@@ -40,9 +60,12 @@ class Atom:
         '''
         if sf_culture not in ISO_639_1_LANGUAGES:
             raise ValueError(f'the language code "{sf_culture}" is not in the ISO 639-1 standard')
-        if not path.lstrip('/').startswith('api/'):
-            raise ValueError(f'the requested API path "{path}" is not an api path!')
-        headers = {'REST-API-Key': self.api_key}
+        if path.lstrip('/').startswith('api/'):
+            if not self.api_key:
+                raise ConnectionError('cannot access AtoM API without an API key set')
+            headers = {'REST-API-Key': self.api_key}
+        else:
+            headers = {}
         if not params:
             params = {}
         if sf_culture:
@@ -51,9 +74,3 @@ class Atom:
         response = self._session.get(request_url, headers=headers, params=params)
         response.raise_for_status()
         return response, request_url
-
-
-class VirtualAtom(Atom):
-    ''' Uses virtual endpoints for accessing authorities and taxonomies. Accesses the front-end as
-    if it was an API, which can be very slow.
-    '''
