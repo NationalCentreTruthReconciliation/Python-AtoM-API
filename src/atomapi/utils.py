e785e50a36d8c07935200b9540525f29394c60c4
Daniel Lovegrove <Daniel.Lovegrove@umanitoba.ca>
2021-03-12 11:27:06 -0600

    Add function to partially format a string

--- src/atomapi/utils.py
+++ src/atomapi/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from urllib3.util.url import parse_url
 
 def parse_url_from_string(url):
@@ -10,3 +12,23 @@ def parse_url_from_string(url):
     if parsed_url.scheme != 'http' and parsed_url.scheme != 'https':
         raise ValueError(f'Could not find http: or https: in "{url}"')
     return parsed_url
+
+
+STR_PARAM = re.compile(r'\{(?P<name>[^\}:]+)(?P<ftype>:[^\}]+)?\}')
+def partial_format(string, **kwargs):
+    ''' Executes a partial string format - only substitutes the parameters specified in the kwargs,
+    and leaves the other string parameters as-is.
+    '''
+    kwarg_dict = dict(kwargs)
+    existing_keys = list(kwarg_dict.keys())
+    new_kwargs = {}
+    for param in STR_PARAM.finditer(string):
+        name = param.group('name')
+        ftype = param.group('ftype')
+        if ftype:
+            msg = f'partial_format does not work with string format types - got "{name + ftype}"'
+            raise ValueError(msg)
+        if name not in existing_keys:
+            new_kwargs[name] = '{'+name+'}'
+    new_kwargs.update(kwarg_dict)
+    return string.format(**new_kwargs)
